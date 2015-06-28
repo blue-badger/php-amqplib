c74f373bc71112e32b1ebbac865aa5ece048a28f
Jonathan <jonathan@badger.blue>
2015-06-27 22:00:38 -0400

    added composer runner

--- runcomposer.sh
+++ runcomposer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /var/loadtest/php-amqp && php /var/loadtest/php-amqp/composer.phar install
+export COMPOSER_HOME="/var/loadtest/php-amqp" && cd /var/loadtest/php-amqp && php /var/loadtest/php-amqp/composer.phar install
