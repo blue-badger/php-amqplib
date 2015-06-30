9bd934f89ae79502de5896b8c8d8c7291018703a
Jonathan <jonathan@badger.blue>
2015-06-29 23:19:47 -0400

    added loadtest folder

---
+++ loadtest/config.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+define('HOST', '45.55.197.70');
+define('PORT', 5672);
+define('USER', 'jonathan');
+define('PASS', 'rfv456TGB^%$');
+define('VHOST', '/');
+
+//If this is enabled you can see AMQP output on the CLI
+define('AMQP_DEBUG', true);
